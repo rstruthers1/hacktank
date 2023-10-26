83aae40e5637931ac2d7b8cc3115669652599fef
Rachel Struthers <rachel.struthers@sap.com>
2023-10-25 20:56:01 -0500

    HKT-25: trying to fix digital ocean deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Root from "./routes/root";
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import RouterErrorPage from "./RouterErrorPage";
 import HomePage from "./components/HomePage";
 
@@ -79,4 +79,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
